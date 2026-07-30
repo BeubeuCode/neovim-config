05766fe637f6ef96c08d445fe3a53a4182d6445c
Benoit Arnoult <benoit.arnoult@team.blue>
2026-07-30 14:22:59 +0200

    add literal grep keymap <leader>sG for special chars

--- init.lua
+++ init.lua
@@ -354,6 +354,9 @@ vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { des
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
+vim.keymap.set('n', '<leader>sG', function()
+  require('telescope.builtin').live_grep { additional_args = { '--fixed-strings' } }
+end, { desc = '[S]earch by [G]rep (literal)' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 
 vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>', {noremap = true})
